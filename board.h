d06818604b3e8a7d8d853f5c01a4066a9675acc7
Jaskirat Singh <jaskirat@Jaskirats-MacBook-Air.local>
2021-08-08 01:34:59 -0400

    board debug

--- board.h
+++ board.h
@@ -2,17 +2,20 @@
 #define __BOARD_H__
 
 #include "cell.h"
+#include "block.h"
 #include <vector>
 
 class Board {
     const int row;
     const int col;
+    int blockCounter;
 
     std::vector<std::vector<Cell>> board;
     
     public:
         Board();
         std::vector<std::vector<Cell>>& getBoard();
+        bool addBlock(Block& block);
         friend std::ostream& operator<<(std::ostream& out, const Board& b);
 };
 
