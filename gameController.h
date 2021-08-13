bad488a19dcbc3f2110e19c59e7ee1c2290f6278
Jaskirat Singh <jaskirat@Jaskirats-MacBook-Air.local>
2021-08-13 03:00:11 -0400

    ..

---
+++ gameController.h
@@ -0,0 +1,20 @@
+#ifndef __GAMECONTROLLER_H__
+#define __GAMECONTROLLER_H__
+
+#include <string>
+#include "game.h"
+#include "view.h"
+
+class GameController {
+    Game player1;
+    Game player2;
+    Game *currPlayer;
+    int currPlayerId;
+    void switchPlayer();
+    View v;
+    public:
+        GameController(int lvl, std::string iF1, std::string iF2);
+        void startGame();
+};
+
+#endif
